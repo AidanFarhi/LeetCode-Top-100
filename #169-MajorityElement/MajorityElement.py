8e346cbec0d11763b9f624681805b8b5370401ac
Aidan Farhi <aidanfarhi@Aidans-MacBook-Air.local>
2021-03-24 19:07:47 -0400

    added boyer moores method for solving majority element

--- #169-MajorityElement/MajorityElement.py
+++ #169-MajorityElement/MajorityElement.py
@@ -4,9 +4,10 @@ class MajorityElement:
     The majority element is the element that appears more than ⌊n / 2⌋ times. 
     You may assume that the majority element always exists in the array.
     """
-    def majorityElement(self, nums) -> int:
-        if len(nums) == 1:
-            return nums[0]
+
+    # This solution is: Time O(N) | Space O(N)
+    def majority_element_with_hashmap(self, nums) -> int:
+        if len(nums) == 1: return nums[0]
         counts = {}
         target = len(nums) // 2
         res = self.get_counts(nums, counts, target)
@@ -14,9 +15,20 @@ class MajorityElement:
 
     def get_counts(self, nums, counts, target):
         for num in nums:
-            if num not in counts:
-                counts[num] = 1
+            if num not in counts: counts[num] = 1
             else:
                 counts[num] += 1
-                if counts[num] > target:
-                    return num
+                if counts[num] > target: return num
+
+    # This solution is: Time O(N) | Space O(1)
+    def majority_element_boyer_moore(self, nums):
+        if len(nums) == 1: return nums[0]
+        candidate = nums[0]
+        counter = 1
+        for i in range(1, len(nums)):
+            if nums[i] == candidate: counter += 1
+            elif nums[i] != candidate and counter > 0: counter -= 1
+            else:
+                candidate = nums[i]
+                counter = 1
+        
